a2096adc66aa3a33dbb926ea6238ce6a8b2bc95c
Gavin Lau <liugf@vip.qq.com>
2012-03-22 01:10:18 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ERP::Application.config.secret_token = 'b5e54374ce153d8a678c420441dd3068cda42c58bbb317870a0b9bc14ffbb1f865291072c9b45e4f73f9d2775ed161d87aa0c34427211125965729dae34ad1ae'
