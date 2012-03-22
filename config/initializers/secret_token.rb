2ea045d795c8780f86519dc2cead013084c7930d
Gavin Lau <liugf@vip.qq.com>
2012-03-22 11:55:59 +0800

    init

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-ERP::Application.config.secret_token = 'b5e54374ce153d8a678c420441dd3068cda42c58bbb317870a0b9bc14ffbb1f865291072c9b45e4f73f9d2775ed161d87aa0c34427211125965729dae34ad1ae'
+ERP::Application.config.secret_token = '744f9866fbe5660ab501be5d48e81c13609908f1c2dbcbfc6148053f6385aedbe0a372e251c91da5f27f7ec1f2032bba6bc277a0becfb8ceb92d22a809526aff'
