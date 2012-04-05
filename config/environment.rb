4c5361ccb22d2c99a10df4524c3ffa57b66a505d
Gavin Lau <liugf@vip.qq.com>
2012-04-05 17:23:23 +0800

    i18n

--- config/environment.rb
+++ config/environment.rb
@@ -6,9 +6,14 @@ require File.expand_path('../application', __FILE__)
 ERP::Application.initialize!
 
 ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
-  if instance.error_message.kind_of?(Array)
-    %(#{html_tag}<span class="input-notification error png_bg">#{instance.error_message.join(',')}</span>).html_safe
+  if !html_tag.start_with? "<label"
+    if instance.error_message.kind_of?(Array)
+      %(#{html_tag}<span class="input-notification error png_bg">#{instance.error_message.join(',')}</span>).html_safe
+    else
+      %(#{html_tag}<span class="input-notification error png_bg">#{instance.error_message}</span>).html_safe
+    end
   else
-    %(#{html_tag}<span class="input-notification error png_bg">#{instance.error_message}</span>).html_safe
+    html_tag
   end
+  
 end
---
+++ lib/tasks/Enterprise_sample_data.rake
@@ -0,0 +1,11 @@
+require 'faker'
+
+namespace :db do
+  desc "fill some sample enterprise in databse"
+  task :enterprise => :environment do
+    20.times do |n|
+            
+     
+    end    
+  end
+end
