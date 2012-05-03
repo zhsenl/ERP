fe96eb01771b11d5e49e44acffaf8a0fd55443b5
Gavin Lau <liugf@vip.qq.com>
2012-05-03 21:31:52 +0800

    print tooltip

--- app/assets/javascripts/print.js
+++ app/assets/javascripts/print.js
@@ -1,21 +1,66 @@
 //= require jquery
 //= require jquery.tools.min
 
-function preview_all(){
-	$(".frame div").css("border-color","#000000");
-	$(".frame div").css("color","#000000");
+function preview_all() {
+	$(".frame div").css("border-color", "#000000");
+	$(".frame div").css("color", "#000000");
 }
 
-function preview_content(){
-	$(".frame div").css("border-color","#ffffff");
-	$(".frame div").css("color","#ffffff");
-	$(".frame .content").css("color","#000000");
+function preview_content() {
+	$(".frame div").css("border-color", "#ffffff");
+	$(".frame div").css("color", "#ffffff");
+	$(".frame .content").css("color", "#000000");
 }
 
-$(document).ready(function(){
-	$(".content").each(function(){
-		if ($(this).html() == ""){
+
+$(document).ready(function() {
+
+	$(".content").each(function() {
+		if($(this).html() == "") {
 			$(this).html("&nbsp;");
-		}		
+		}
 	});
+	
+	$(".content").click(function(){
+		$(this).after('<div class="tooltip">' + $('#mytip').html() + '</div>');
+	});
+
+	$(".content").tooltip({
+		events : {
+			def : "click,mouseleave", // default show/hide events for an element
+			input : "focus,blur", // for all input elements
+			widget : "focus mouseenter,blur mouseleave", // select, checkbox, radio, button
+			tooltip : "mouseenter,mouseleave"     // the tooltip element
+		},
+		onShow : function(){	
+			var textarea = this.getTip().find("#textarea");
+			var inner_text = this.getTrigger().html();
+			if (inner_text == "&nbsp;") {
+				textarea.val("");
+			} else {
+				textarea.val(inner_text);
+			}
+			textarea.focus();			
+		},
+		onHide : function(){
+			var textarea = this.getTip().find("#textarea");
+			var inner_text = textarea.val();
+			if (inner_text == "" ) {
+				inner_text = "&nbsp;";
+			};
+			this.getTrigger().html(inner_text);
+			this.getTrigger().parent().find(".tooltip").each(function(){
+				var position = $(this).css("position")
+				if ($(this).css("position") != "absolute") {
+					$(this).detach();
+				}				
+			});
+		}
+	}).dynamic({
+		bottom : {
+			direction : 'down',
+			bounce : true
+		}
+	});
+	
 });
