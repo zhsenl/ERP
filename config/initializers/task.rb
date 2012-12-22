ce4444054f5b879f87e84ef0aedde973114807e6
Gavin Lau <liugf@vip.qq.com>
2012-12-22 16:31:47 +0800

    dispatcher

--- config/initializers/task.rb
+++ config/initializers/task.rb
@@ -6,6 +6,6 @@ scheduler.every("2m") do
    system('rake receipt:check_receipts')
 end
 
-scheduler.every("10s") do
+scheduler.every("2m") do
    system("ruby #{Rails.root.join('lib', 'tasks', "dispatcher.rb")}")
 end
