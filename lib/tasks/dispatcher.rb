fb834b20dd63b9be1b9ad1df3205e241e11b174c
Gavin Lau <liugf@vip.qq.com>
2012-12-22 13:36:54 +0800

    dispatcher

--- lib/tasks/dispatcher.rb
+++ lib/tasks/dispatcher.rb
@@ -11,7 +11,7 @@ Dir.new(Settings['dispatch_paths']['upload_temp']).each do |file_name|
   if file_name != '.' and file_name != '..'
     message_file = File.new(Settings['dispatch_paths']['upload_temp'] + '/' + file_name)
     begin
-      Net::FTP.open(Settings['ftp_info']['host'], Settings['ftp_info']['username'], Settings['ftp_info']['password']) do |ftp|
+      Net::FTP.open(Settings['ftp_info']['host'], Settings['ftp_info']['username'], Settings['ftp_info']['password'].to_s) do |ftp|
         ftp.chdir('TCS/UploadTemp')
         ftp.puttextfile(message_file.path) 
         ftp.rename(file_name, '../Upload/' + file_name)           
@@ -28,7 +28,7 @@ Dir.new(Settings['dispatch_paths']['upload_temp']).each do |file_name|
 end
 
 #接收报文
-Net::FTP.open(Settings['ftp_info']['host'], Settings['ftp_info']['username'], Settings['ftp_info']['password']) do |ftp|
+Net::FTP.open(Settings['ftp_info']['host'], Settings['ftp_info']['username'], Settings['ftp_info']['password'].to_s) do |ftp|
   ftp.nlst('TCS/Download').each do |file|
   	temp_file = Settings['dispatch_paths']['download_temp'] + '/' + File.basename(file)
     ftp.gettextfile(file, temp_file)
