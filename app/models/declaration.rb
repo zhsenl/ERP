5f39c691921366513fd8dfd3758431f3a32ec7a2
Gavin Lau <liugf@vip.qq.com>
2013-01-12 23:31:53 +0800

    fixbug

--- app/models/declaration.rb
+++ app/models/declaration.rb
@@ -25,6 +25,7 @@ class Declaration < ActiveRecord::Base
   validates :gross_weight, :presence => true, :numericality => true
   validates :net_weight, :presence => true, :numericality => true
   validates :transit_type, :presence => true
+  validates :transport_tool, :presence => true, :length => { :maximum => 14 }
 
   before_validation :process_data
 
