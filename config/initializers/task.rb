95211366f64f0a657ee7ae35d1e5efbded099b8a
Gavin Lau <liugf@vip.qq.com>
2013-03-13 23:12:00 +0800

    auto config task

--- config/initializers/task.rb
+++ config/initializers/task.rb
@@ -2,10 +2,12 @@ require 'rubygems'
 require 'rufus/scheduler'
 scheduler = Rufus::Scheduler.start_new
 
-scheduler.every("2m") do
-   system('rake receipt:check_receipts')
-end
+if Rails.env == "production"
+	scheduler.every("2m") do
+	   system('rake receipt:check_receipts')
+	end
 
-scheduler.every("90s") do
-   system("ruby #{Rails.root.join('lib', 'tasks', "dispatcher.rb")}")
+	scheduler.every("90s") do
+	   system("ruby #{Rails.root.join('lib', 'tasks', "dispatcher.rb")}")
+	end
 end
