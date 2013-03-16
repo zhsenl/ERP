18fccb4e0f7756172f3f71eac8c53100954f4ac7
Gavin Lau <liugf@vip.qq.com>
2013-03-16 16:14:11 +0800

    change task mode

---
+++ lib/task.rb
@@ -0,0 +1,15 @@
+require 'rubygems'
+require 'rufus/scheduler'
+scheduler = Rufus::Scheduler.start_new
+
+rails_root = File.expand_path('../../', __FILE__)
+
+scheduler.every("2m") do
+   system('rake receipt:check_receipts')
+end
+
+scheduler.every("90s") do
+   system("ruby #{rails_root +'/lib/tasks/dispatcher.rb'}")
+end
+
+scheduler.join
---
+++ start.bat
@@ -0,0 +1,5 @@
+cd /d %~dp0
+start /b thin start -p 3000 -e production
+start /b thin start -p 3001 -e production
+start /b thin start -p 3002 -e production
+ruby lib/task.rb
