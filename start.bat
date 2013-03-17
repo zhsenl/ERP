ddd543a07807ea32e36d0448ee5fb31272c97cf6
Gavin Lau <liugf@vip.qq.com>
2013-03-17 12:10:24 +0800

    mutil thin

--- start.bat
+++ start.bat
@@ -2,4 +2,6 @@ cd /d %~dp0
 start /b thin start -p 3000 -e production
 start /b thin start -p 3001 -e production
 start /b thin start -p 3002 -e production
+start /b thin start -p 3003 -e production
+start /b thin start -p 3004 -e production
 ruby lib/task.rb
