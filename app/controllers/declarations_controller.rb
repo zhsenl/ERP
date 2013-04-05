eaff0832833d0f5ee013ae787108332a4457a0f2
Gavin Lau <liugf@vip.qq.com>
2013-04-06 00:44:17 +0800

    driver paper

--- app/controllers/declarations_controller.rb
+++ app/controllers/declarations_controller.rb
@@ -8,7 +8,7 @@ class DeclarationsController < ApplicationController
       @declaration = Declaration.find(params[:id])
       @declaration_type = @declaration.declaration_type
     else
-      @declaration_type = params[:declaration_type] || params[:declaration][:declaration_type]
+      @declaration_type = params[:declaration_type] || params[:declaration][:declaration_type] rescue nil
     end
     @mark = @declaration_type    
   end
@@ -36,8 +36,77 @@ class DeclarationsController < ApplicationController
       format.html # show.html.erb
       format.json { render json: @declaration }
     end
+  end  
+
+  # GET /declarations/new
+  # GET /declarations/new.json
+  def new
+    if current_enterprise
+      pre_entry_no = Time.now.strftime('%Y%m%d%H%M%S') + system_serial_no
+      @declaration = Declaration.new(:enterprise_id => current_enterprise.id,
+                                      :declaration_type => @declaration_type,
+                                      :pre_entry_no => pre_entry_no,
+                                      :pay_way => "7",
+                                      :deal_mode => @declaration_type == "export" ? "3" : "1",
+                                      :declare_enterprise_code => "4419980074",
+                                      :transit_type => "001",
+                                      :created_by => current_user.username)
+      @declaration.declaration_transit_information = DeclarationTransitInformation.new(:local_transport_mode => 4)
+    else
+      redirect_to declarations_path(:declaration_type => @declaration_type), notice: '请选择要操作的企业'
+    end
   end
-  
+
+  # GET /declarations/1/edit
+  def edit
+    authorize! :edit, @declaration
+  end
+
+  # POST /declarations
+  # POST /declarations.json
+  def create
+    @declaration = Declaration.new(params[:declaration])
+    authorize! :create, @declaration
+
+    respond_to do |format|
+      if @declaration.save
+        format.html { redirect_to @declaration, :flash => { :success => '成功保存报关单'} }
+        format.json { render json: @declaration, status: :created, location: @declaration }
+      else
+        format.html { render action: "new", :declaration_type => 1 }
+        format.json { render json: @declaration.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PUT /declarations/1
+  # PUT /declarations/1.json
+  def update
+    authorize! :update, @declaration
+    respond_to do |format|
+      if @declaration.update_attributes(params[:declaration])
+        format.html { redirect_to @declaration, :flash => { :success => '成功修改报关单'} }
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @declaration.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /declarations/1
+  # DELETE /declarations/1.json
+  def destroy
+    authorize! :destory, @declaration
+    @declaration.destroy
+
+    respond_to do |format|
+      format.html { redirect_to declarations_url(:declaration_type => @declaration_type), :flash => { :success => '删除成功'}}
+      format.json { head :no_content }
+    end
+  end
+
+
   def print_declaration 
     authorize! :show, @declaration
     @declaration_cargos = @declaration.declaration_cargos.order("no")
@@ -111,71 +180,16 @@ class DeclarationsController < ApplicationController
     end
   end
 
-  # GET /declarations/new
-  # GET /declarations/new.json
-  def new
-    if current_enterprise
-      pre_entry_no = Time.now.strftime('%Y%m%d%H%M%S') + system_serial_no
-      @declaration = Declaration.new(:enterprise_id => current_enterprise.id,
-                                      :declaration_type => @declaration_type,
-                                      :pre_entry_no => pre_entry_no,
-                                      :pay_way => "7",
-                                      :deal_mode => @declaration_type == "export" ? "3" : "1",
-                                      :declare_enterprise_code => "4419980074",
-                                      :transit_type => "001",
-                                      :created_by => current_user.username)
-      @declaration.declaration_transit_information = DeclarationTransitInformation.new(:local_transport_mode => 4)
-    else
-      redirect_to declarations_path(:declaration_type => @declaration_type), notice: '请选择要操作的企业'
-    end
+  def driver_paper
+
   end
 
-  # GET /declarations/1/edit
-  def edit
-    authorize! :edit, @declaration
+  def print_driver_paper_1
+
   end
 
-  # POST /declarations
-  # POST /declarations.json
-  def create
-    @declaration = Declaration.new(params[:declaration])
-    authorize! :create, @declaration
+  def print_driver_paper_2
 
-    respond_to do |format|
-      if @declaration.save
-        format.html { redirect_to @declaration, :flash => { :success => '成功保存报关单'} }
-        format.json { render json: @declaration, status: :created, location: @declaration }
-      else
-        format.html { render action: "new", :declaration_type => 1 }
-        format.json { render json: @declaration.errors, status: :unprocessable_entity }
-      end
-    end
   end
 
-  # PUT /declarations/1
-  # PUT /declarations/1.json
-  def update
-    authorize! :update, @declaration
-    respond_to do |format|
-      if @declaration.update_attributes(params[:declaration])
-        format.html { redirect_to @declaration, :flash => { :success => '成功修改报关单'} }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @declaration.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /declarations/1
-  # DELETE /declarations/1.json
-  def destroy
-    authorize! :destory, @declaration
-    @declaration.destroy
-
-    respond_to do |format|
-      format.html { redirect_to declarations_url(:declaration_type => @declaration_type), :flash => { :success => '删除成功'}}
-      format.json { head :no_content }
-    end
-  end
 end
