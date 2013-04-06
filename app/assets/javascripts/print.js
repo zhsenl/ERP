cdac19723be192238d1e65a5611ef1cb54549ae7
zhsenl <zhsenl@qq.com>
2013-04-06 19:52:16 +0800

    fix bug -- print contract

--- app/assets/javascripts/print.js
+++ app/assets/javascripts/print.js
@@ -28,11 +28,15 @@ $(document).ready(function() {
 		}
 	});
 	
-	$(".content").click(function(){
+	$(".content").not(".toggle").click(function(){
 		$(this).after('<div class="tooltip">' + $('#mytip').html() + '</div>');
 	});
 
-	$(".content").tooltip({
+    $(".toggle").click(function(){
+        this.innerHTML = this.innerHTML == '&nbsp;' ? '√' : '&nbsp;'
+    })
+
+	$(".content").not(".toggle").tooltip({
 		events : {
 			def : "click,mouseleave", // default show/hide events for an element
 			input : "focus,blur", // for all input elements
