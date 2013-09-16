4c207fe5bed484d67f20f0ad0b30a156d10e6784
zhsenl <zhsenl@qq.com>
2013-09-16 10:46:02 +0800

    在命令中加入 bundle exec

--- lib/task.rb
+++ lib/task.rb
@@ -5,13 +5,13 @@ scheduler = Rufus::Scheduler.start_new
 rails_root = File.expand_path('../../', __FILE__)
 
 scheduler.every("2m") do
-   system('rake receipt:check_receipts')
-   system('rake receipt:app_bill_check_receipts')
+   system('bundle exec rake receipt:check_receipts')
+   system('bundle exec rake receipt:app_bill_check_receipts')
 end
 
 scheduler.every("90s") do
-   system("ruby #{rails_root +'/lib/tasks/dispatcher.rb'}")
-   system("ruby #{rails_root +'/lib/tasks/app_bill_dispatcher.rb'}")
+   system("bundle exec ruby #{rails_root +'/lib/tasks/dispatcher.rb'}")
+   system("bundle exec ruby #{rails_root +'/lib/tasks/app_bill_dispatcher.rb'}")
 end
 
 scheduler.join
