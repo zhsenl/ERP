3e93d9297974333aab2d32d6e4f5bff2b68ca9f8
zhsenl <zhsenl@qq.com>
2013-09-16 12:07:51 +0800

    分开dispatcher和check

--- lib/task.rb
+++ lib/task.rb
@@ -4,11 +4,6 @@ scheduler = Rufus::Scheduler.start_new
 
 rails_root = File.expand_path('../../', __FILE__)
 
-scheduler.every("2m") do
-   system('bundle exec rake receipt:check_receipts')
-   system('bundle exec rake receipt:app_bill_check_receipts')
-end
-
 scheduler.every("90s") do
    system("bundle exec ruby #{rails_root +'/lib/tasks/dispatcher.rb'}")
    system("bundle exec ruby #{rails_root +'/lib/tasks/app_bill_dispatcher.rb'}")
