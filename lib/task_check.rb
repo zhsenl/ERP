ba530216abed2f2e3fa2d2c060f19d744d0179fe
zhsenl <zhsenl@qq.com>
2013-11-20 21:25:15 +0800

    加上显示发送接收报文和更新数据库中报文的状态的时间戳，去掉app_bill的发送接收task

--- lib/task_check.rb
+++ lib/task_check.rb
@@ -6,7 +6,7 @@ rails_root = File.expand_path('../../', __FILE__)
 
 scheduler.every("2m") do
   system('bundle exec rake receipt:check_receipts')
-  system('bundle exec rake receipt:app_bill_check_receipts')
+  #system('bundle exec rake receipt:app_bill_check_receipts')
 end
 
 scheduler.join
