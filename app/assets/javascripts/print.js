ba8ecc5c2ce581a76f695cbeb2c470389d7e7122
zhsenl <zhsenl@qq.com>
2014-01-21 21:45:46 +0800

    印章

--- app/assets/javascripts/print.js
+++ app/assets/javascripts/print.js
@@ -32,7 +32,7 @@ function html_encode(text){
 }
 
 $(document).ready(function() {
-    $('.seal').css('display','none');
+    //$('.seal').css('display','none');
 
 	$(".content").each(function() {
 		if($(this).html() == "") {
