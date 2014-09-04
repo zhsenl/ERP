27d1999d0445187bc3e1d1fb13de5667eb817f7a
zhsenl <zhsenl@qq.com>
2014-09-04 18:12:01 +0800

    fix bug

--- app/controllers/checkout_enterprises_controller.rb
+++ app/controllers/checkout_enterprises_controller.rb
@@ -42,7 +42,8 @@ class CheckoutEnterprisesController < ApplicationController
   # POST /checkout_enterprises.json
   def create
     @checkout_enterprise = CheckoutEnterprise.new(params[:checkout_enterprise])
-
+    @checkout_enterprise.save
+    create_finance_fee   #创建在付费信息里填写的结算单位对应的账单信息
     respond_to do |format|
       if @checkout_enterprise.save
         create_finance_fee   #创建在付费信息里填写的结算单位对应的账单信息
--- app/models/checkout_enterprise.rb
+++ app/models/checkout_enterprise.rb
@@ -1,5 +1,5 @@
 class CheckoutEnterprise < ActiveRecord::Base
   has_many :finance_fees
-  validates :code, :presence => true, :length => { :is => 10 }, :numericality => true
+  validates :code, :presence => true, :length => { :is => 10 }#, :numericality => true
   validates_uniqueness_of :code, :scope => :finance_id
 end
