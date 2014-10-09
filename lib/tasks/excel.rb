7a8bd0a437093b69f666f6c79bcdd7dfbc1a7dbc
zhsenl <zhsenl@qq.com>
2014-10-09 16:24:52 +0800

    fix bug

---
+++ lib/tasks/excel.rb
@@ -0,0 +1,17 @@
+
+require 'win32ole'
+excel = nil
+begin
+  excel = WIN32OLE.connect("excel.application")
+rescue
+  excel = WIN32OLE.new("excel.application")
+end
+excel.visible = true
+puts ARGV[0]
+workbook = excel.Workbooks.Open(ARGV[0])
+filename =  ARGV[0][0..-5] + '_excel.xls'
+dos_file = filename.gsub(/\//, "\\\\")
+workbook.SaveAs dos_file, 56
+excel.ActiveWorkbook.Close(0);
+
+
